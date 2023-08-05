7b71f24457c49895da010d01f3d457a2207eff2d
Andy Nguyen <nguyen3andy@gmail.com>
2023-08-05 01:26:59 -0400

    files worked on

--- models/comment.js
+++ models/comment.js
@@ -1,38 +1,19 @@
-const { Model, DataTypes } = require('sequelize');
+
+const { Sequelize, Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
 class Comment extends Model {}
-Comment.init({
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    comment_text: {
-        type: DataTypes.STRING,
-        validate: {
-            len: [3]
-        }
-    },
-    user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'user',
-            key: 'id'
-        }
-    },
-    post_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'post',
-            key: 'id'
-        }
+
+Comment.init(
+  {
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
-}, {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'comment'
-});
+  },
+  {
+    sequelize
+  }
+);
+
 module.exports = Comment;
